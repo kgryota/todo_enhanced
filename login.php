9c138599a9f8f08d32e1b7287f038cb36c3b35f6
sasakiasuma <2301164@s.asojuku.ac.jp>
2025-06-24 10:38:01 +0900

    ログイン画面

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="/view.php" method="post">
+        <label for="username">ユーザー名：</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">パスワード：</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit">ログイン</button>
+    </form>
+    <a href="register.php">新規登録</a>
+</body>
+</html>
