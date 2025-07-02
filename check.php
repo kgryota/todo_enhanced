011027296efa3771f0c2e4fe0ffe5a2785a8a1cc
sasakiasuma <2301164@s.asojuku.ac.jp>
2025-07-02 11:53:39 +0900

    Update check.php

--- check.php
+++ check.php
@@ -1,30 +1,35 @@
 <?php
-if(isset($_POST['username'])){
-     $user_name = $_POST['username'];
-   }
+session_start();
 
-   if(isset($_POST['password'])){
-     $user_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-   }
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
+if ($username === '' || $password === '') {
+    header("Location: login.php?error=empty");
+    exit;
+}
 
- try {
-      // データベースに接続
+try {
+    $pdo = new PDO(
+        'mysql:host=mysql321.phy.lolipop.lan;dbname=LAA1554899-todoapp;charset=utf8',
+        'LAA1554899',
+        'teamproject'
+    );
+} catch(PDOException $e){
+    die('接続できませんでした：'.$e->getMessage());
+}
 
-$pdo = new PDO(
-    'mysql:host=mysql321.phy.lolipop.lan;dbname=LAA1554899-todoapp;charset=utf8',
-    'LAA1554899',
-    'teamproject'
-);
-}catch(PDOException $e){
-          echo '接続できませんでした。理由：'.$e->getMessage();
-      }
+$stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->execute([$username]);
+$user = $stmt->fetch();
 
-      if($user === false || password_verify($password, $user['password']) === false){
-          echo 'ログインできませんでした';
-        }
-        else{
-          echo 'ログイン成功';
-        }
-
+if ($user && password_verify($password, $user['password'])) {
+    $_SESSION['user_name'] = $user['username'];
+    $_SESSION['admin_login'] = true;
+    header("Location: index.php");
+    exit;
+} else {
+    header("Location: login.php?error=invalid");
+    exit;
+}
 ?>
